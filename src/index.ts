6b8be1ec2993bd2fbbfc51816a32d73489470c7a
moyarich <moyarich@hotmail.com>
2026-09-25 17:14:58 -0500

    feat: expose parser utilities from package root

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 export { extractCssColors } from "./extractCssColors";
 export { CSS_NAMED_COLORS } from "./namedColors";
-export { parseCssColor } from "./parseCssColor";
-export type { ParsedCssColor } from "./parseCssColor";
+export {
+  hslToRgb,
+  parseAlpha,
+  parseCssColor,
+  parseFunctionalComponents,
+  parseHexColor,
+  parseHslColor,
+  parseHue,
+  parseNamedColor,
+  parsePercentage,
+  parseRgbChannel,
+  parseRgbColor,
+} from "./parseCssColor";
+export type { FunctionalComponents, ParsedCssColor } from "./parseCssColor";
 export type { CssColorFormat, CssColorMatch, RgbaColor } from "./types";
